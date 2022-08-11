68380aef1ea4dbf2c79305b31c72a4497ec4fc38
Lance Gee <Lancegee.apps@gmail.com>
2022-08-11 10:22:18 -0600

    Updates

---
+++ precache-manifest.74813ba7511c0eb0ebbdebffa047f9a5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "f3fd26b9b804cbdc5201",
+    "url": "/Lance-Gee/newCityBakeryReactPage/static/css/main.6c89aae8.chunk.css"
+  },
+  {
+    "revision": "f3fd26b9b804cbdc5201",
+    "url": "/Lance-Gee/newCityBakeryReactPage/static/js/main.f3fd26b9.chunk.js"
+  },
+  {
+    "revision": "55de65e8354a778fb23a",
+    "url": "/Lance-Gee/newCityBakeryReactPage/static/css/1.01fee5cb.chunk.css"
+  },
+  {
+    "revision": "55de65e8354a778fb23a",
+    "url": "/Lance-Gee/newCityBakeryReactPage/static/js/1.55de65e8.chunk.js"
+  },
+  {
+    "revision": "07044fb16453d9d84742",
+    "url": "/Lance-Gee/newCityBakeryReactPage/static/js/runtime~main.07044fb1.js"
+  },
+  {
+    "revision": "840b9472f9237860dbe3ba7d7e2a0c00",
+    "url": "/Lance-Gee/newCityBakeryReactPage/static/media/menu_background.755b4fd1.jpg"
+  },
+  {
+    "revision": "3b23488d14c6032d1d803917942f64ef",
+    "url": "/Lance-Gee/newCityBakeryReactPage/static/media/pattern_bg.e14cf174.png"
+  },
+  {
+    "revision": "ec5e9f9677dbb7650f036057fd9e189e",
+    "url": "/Lance-Gee/newCityBakeryReactPage/static/media/Logo.fd2585eb.png"
+  },
+  {
+    "revision": "424f4eac68117ed38b2bc65d305d8e32",
+    "url": "/Lance-Gee/newCityBakeryReactPage/index.html"
+  }
+];
